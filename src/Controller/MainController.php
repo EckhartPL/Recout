cc70a30c85f15d2ce6888bb9941cf141a51661ef
Mateusz Masek <mmasek@szynaka.pl>
2020-08-05 14:11:59 +0200

    Updating MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Reviewer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,5 +15,24 @@ class MainController extends AbstractController
     public function index()
     {
         return $this->render('main_page/main.html.twig');
+    }
+
+    /**
+     * @Route("/reviewer", name="create_reviewer")
+     */
+    public function createReviewer() : Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $reviewer = new Reviewer();
+        $reviewer->setName('Mateusz');
+        $reviewer->setSurname('Masek');
+        $reviewer->setPassword('qwerty12345');
+        $reviewer->setMail('mateusz.masek@wp.pl');
+
+        $entityManager->persist($reviewer);
+        $entityManager->flush();
+
+        return new Response('Saved new Reviewer with ID: '.$reviewer->getId());
     }
 }
